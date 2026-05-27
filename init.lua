313ba16841da5822d387e5015f8d49102b0fadd9
cultab <rroarck@gmail.com>
2026-05-27 21:21:10 +0300

    lsp for python

--- init.lua
+++ init.lua
@@ -7,6 +7,45 @@ vim.g.command = {
 	icon = require('user.icons').misc.term,
 }
 
-vim.pack.add( {
+vim.pack.add {
 	'https://github.com/cultab/command.nvim', -- my plugin
+}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+if capabilities.workspace then
+
+  capabilities.workspace.didChangeWatchedFiles = nil
+
+end
+
+vim.lsp.codelens.enable()
+vim.lsp.inlay_hint.enable()
+vim.lsp.config('*', {
+
+  capabilities = capabilities,
+
 })
+
+vim.lsp.config['ruff'] = {
+	cmd = { 'ruff', 'server' },
+	filetypes = { 'python' },
+	root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
+	settings = {},
+}
+
+vim.lsp.config['jedi-language-server'] = {
+	cmd = { 'jedi-language-server' },
+	filetypes = { 'python' },
+	root_markers = {
+		'pyproject.toml',
+		'setup.py',
+		'setup.cfg',
+		'requirements.txt',
+		'Pipfile',
+		'.git',
+	},
+}
+
+vim.lsp.enable 'ruff'
+vim.lsp.enable 'jedi-language-server'
